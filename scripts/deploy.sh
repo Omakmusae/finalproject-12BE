ceaa1bfce84f1feef88795566e2b1a5eedeab227
ãyoobeommo <kiekangmin@naver.com>
2023-06-01 01:16:47 +0900

    Fix : cicd 관련 오류 수정 48

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,36 +1,23 @@
 #!/usr/bin/env bash
 
-
-
 REPOSITORY=/home/ubuntu/finalproject12Be
 cd $REPOSITORY
 
 APP_NAME=finalproject12Be
-JAR_NAME=finalproject12Be-0.0.1-SNAPSHOT.jar
-JAR_PATH=$REPOSITORY/build/libs/finalproject12Be-0.0.1-SNAPSHOT.jar
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl $APP_NAME | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z "$CURRENT_PID" ];
+if [ -z $CURRENT_PID ]
 then
   echo "> 종료할 애플리케이션이 없습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo "> kill -9 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
-
-JAR_PATH=$REPOSITORY/build/libs/finalproject12Be-0.0.1-SNAPSHOT.jar
-
-echo "> JAR NAME: $JAR_PATH"
-
-echo "> $JAR_PATH 에 실행권한 추가"
-
-chmod +x $JAR_PATH
-
-echo "> $JAR_PATH 실행"
-
-nohup java -jar $JAR_PATH >> $REPOSITORY/nohup.out 2>&1 &
+echo "> Deploy - $JAR_PATH "
+export JASYPT_ENCRYPTOR_PASSWORD=finalPJ
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
