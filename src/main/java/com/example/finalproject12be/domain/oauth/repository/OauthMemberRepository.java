24c86bc643dc6bfb6998856b332c2be10095b130
DESKTOP-8BAC7IL\pak92 <pak926@nate.com>
2023-06-01 21:14:59 +0900

    Feat : 카카오 소셜 로그인 기능 구현 - Redirect URI 변경

--- src/main/java/com/example/finalproject12be/domain/oauth/repository/OauthMemberRepository.java
+++ src/main/java/com/example/finalproject12be/domain/oauth/repository/OauthMemberRepository.java
@@ -10,4 +10,5 @@ import com.example.finalproject12be.domain.oauth.entity.OauthMember;
 public interface OauthMemberRepository extends JpaRepository<OauthMember, Long> {
 	Optional<OauthMember> findByEmail(String email);
 	Optional<OauthMember> findByNickname(String nickname);
+	Optional<OauthMember> findById(Long id);
 }
