80bc7ff463776284dde8d58e7d47316f274005e6
kes <kmskes1125@gmail.com>
2023-06-01 20:51:43 +0900

    FEAT: 북마크 기능 추가

--- src/main/java/com/example/finalproject12be/domain/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/example/finalproject12be/domain/bookmark/repository/BookmarkRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.finalproject12be.domain.bookmark.entity.Bookmark;
+import com.example.finalproject12be.domain.member.entity.Member;
 import com.example.finalproject12be.domain.store.entity.Store;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
@@ -14,4 +15,6 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 	Optional<Bookmark> findByStore(Store store);
 
 	Optional<Bookmark> findByStoreId(Long storeId);
+
+	Optional<Bookmark> findByStoreAndMember(Store store, Member member);
 }
--- src/main/java/com/example/finalproject12be/domain/store/controller/StoreController.java
+++ src/main/java/com/example/finalproject12be/domain/store/controller/StoreController.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.concurrent.Flow;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.example.finalproject12be.domain.store.OpenApiManager;
 import com.example.finalproject12be.domain.store.dto.OneStoreResponseDto;
 import com.example.finalproject12be.domain.store.dto.StoreResponseDto;
 import com.example.finalproject12be.domain.store.service.StoreService;
+import com.example.finalproject12be.security.UserDetailsImpl;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,23 +30,26 @@ public class StoreController {
 	private final StoreService storeService;
 
 	@GetMapping("/api/store")
-	public List<StoreResponseDto> getAllStores(){
-		return storeService.getAllStores();
+	public List<StoreResponseDto> getAllStores(@AuthenticationPrincipal UserDetailsImpl userDetails){
+		return storeService.getAllStores(userDetails);
 	}
 
 	@GetMapping("/api/store/{id}")
-	public OneStoreResponseDto getStore(@PathVariable(name = "id") Long storeId){
-		return storeService.getStore(storeId);
+	public OneStoreResponseDto getStore(
+		@PathVariable(name = "id") Long storeId,
+		@AuthenticationPrincipal UserDetailsImpl userDetails){
+		return storeService.getStore(storeId, userDetails);
 	}
 
 	@GetMapping("/api/store/search")
 	public List<StoreResponseDto> searchStore(
-			@RequestParam("storeName") String storeName,
-			@RequestParam("gu") String gu,
-			@RequestParam("open") boolean open,
-			@RequestParam("holidayBusiness") boolean holidayBusiness,
-			@RequestParam("nightBusiness") boolean nightBusiness){
-		return storeService.searchStore(storeName, gu, open, holidayBusiness, nightBusiness);
+		@RequestParam("storeName") String storeName,
+		@RequestParam("gu") String gu,
+		@RequestParam("open") boolean open,
+		@RequestParam("holidayBusiness") boolean holidayBusiness,
+		@RequestParam("nightBusiness") boolean nightBusiness,
+		@AuthenticationPrincipal UserDetailsImpl userDetails){
+		return storeService.searchStore(storeName, gu, open, holidayBusiness, nightBusiness, userDetails);
 	}
 
 
