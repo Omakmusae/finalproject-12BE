c65b287426b014bf9c3f65a748f571c5e8622920
kes <kmskes1125@gmail.com>
2023-06-04 05:11:05 +0900

    Feat: 기존 약국 DB에 외국어 가능 컬럼 추가

--- src/main/java/com/example/finalproject12be/domain/store/repository/StoreRepository.java
+++ src/main/java/com/example/finalproject12be/domain/store/repository/StoreRepository.java
@@ -1,6 +1,7 @@
 package com.example.finalproject12be.domain.store.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.finalproject12be.domain.store.entity.Store;
@@ -18,4 +19,6 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
     List<Store> findAllBysundayTimeContaining(String time);
 
     List<Store> findAllByNameContaining(String storeName);
+
+    Optional<Store> findByCallNumber(Object callNumber);
 }
