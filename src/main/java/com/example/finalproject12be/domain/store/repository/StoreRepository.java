f35f630967796c9f6c2b7598b7497e8f429d09a4
DESKTOP-8BAC7IL\pak92 <pak926@nate.com>
2023-06-04 22:03:25 +0900

    feat: 사용자 위치 기반 지도 서비스 구현 - 사용자 위도, 경도, 반경, address 매개변수를 기반하여 조건에 맞는 약국 조회 - 이를 위해 Store 엔티티의 위도, 경도 타입을 String 에서 Double로 변경

--- src/main/java/com/example/finalproject12be/domain/store/repository/StoreRepository.java
+++ src/main/java/com/example/finalproject12be/domain/store/repository/StoreRepository.java
@@ -1,8 +1,12 @@
 package com.example.finalproject12be.domain.store.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.example.finalproject12be.domain.store.entity.Store;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
