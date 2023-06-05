8f1a0527028cb939e22097662c29d848d28d7924
ãyoobeommo <kiekangmin@naver.com>
2023-06-05 22:10:49 +0900

    Fix : 댓글 100자 제한 유효성 추가 / cicd 오류 수정

--- src/main/java/com/example/finalproject12be/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/example/finalproject12be/domain/comment/dto/CommentRequestDto.java
@@ -1,16 +1,16 @@
 package com.example.finalproject12be.domain.comment.dto;
 
-
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor
 public class CommentRequestDto {
 
     @NotBlank
+    @Size(max = 100)
     private String contents;
 }
