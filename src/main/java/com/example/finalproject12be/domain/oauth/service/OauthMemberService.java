75d115dde764debcccbd3f1ce83ed6ed3c543e85
DESKTOP-8BAC7IL\pak92 <pak926@nate.com>
2023-06-07 00:06:07 +0900

    feat: 소셜 로그인 redirect-uri 주소를 배포 주소로 변경

--- src/main/java/com/example/finalproject12be/domain/oauth/service/OauthMemberService.java
+++ src/main/java/com/example/finalproject12be/domain/oauth/service/OauthMemberService.java
@@ -88,10 +88,10 @@ public class OauthMemberService {
 		// HTTP Body 생성
 		MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
 		body.add("grant_type", "authorization_code");
-		body.add("client_id", "048f9445160611c1cc986c481c2d6b94");//내 앱 rest api 키
-		//body.add("client_id", "7463ed7e96bc168b9023480e535add90");//오디약 rest api 키
-		body.add("redirect_uri", "http://localhost:8080/user/signin/kakao");
-		//body.add("redirect_uri", "https://finalproject-12-fe.vercel.app/user/signin/kakao");//오디약 redirect url
+		//body.add("client_id", "048f9445160611c1cc986c481c2d6b94");//내 앱 rest api 키
+		body.add("client_id", "7463ed7e96bc168b9023480e535add90");//오디약 rest api 키
+		//body.add("redirect_uri", "http://localhost:8080/user/signin/kakao");
+		body.add("redirect_uri", "https://finalproject-12-fe.vercel.app/user/signin/kakao");//오디약 redirect url
 		body.add("code", code);
 
 		// HTTP 요청 보내기
