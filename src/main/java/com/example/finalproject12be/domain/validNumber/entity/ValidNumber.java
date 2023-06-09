bd015c26c59acde8d356de5008b4f1d96062417e
kes <kmskes1125@gmail.com>
2023-06-10 01:44:04 +0900

    Feat: 이메일 인증 및 인증번호 검사 로직 구현

---
+++ src/main/java/com/example/finalproject12be/domain/validNumber/entity/ValidNumber.java
@@ -0,0 +1,40 @@
+package com.example.finalproject12be.domain.validNumber.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.example.finalproject12be.domain.member.entity.Member;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class ValidNumber {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "VALIDNUMBER_ID")
+	private Long id;
+
+	@Column
+	private int validNumber;
+
+	@Column
+	private String email;
+
+	@Column
+	private double time;
+
+	public ValidNumber(int validNumber, String email, double time) {
+		this.validNumber = validNumber;
+		this.email = email;
+		this.time = time;
+	}
+}
