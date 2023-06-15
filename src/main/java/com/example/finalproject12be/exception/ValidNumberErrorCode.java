72deabc0ea3a79d1a2f85c19f15a5e3c509eb6da
kes <kmskes1125@gmail.com>
2023-06-16 02:49:31 +0900

    Fix: 이메일 인증번호 틀릴 시 에러코드 각자 다르게 반환

---
+++ src/main/java/com/example/finalproject12be/exception/ValidNumberErrorCode.java
@@ -0,0 +1,18 @@
+package com.example.finalproject12be.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ValidNumberErrorCode implements ErrorCode {
+
+	VALID_TIME_OVER(HttpStatus.BAD_REQUEST, "만료시간이 지난 인증번호입니다"),
+	WRONG_NUMBER(HttpStatus.BAD_REQUEST, "인증번호가 틀렸습니다")
+	;
+
+	private final HttpStatus httpStatus;
+	private final String message;
+}
